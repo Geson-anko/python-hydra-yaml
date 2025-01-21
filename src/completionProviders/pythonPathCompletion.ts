19a69282a5b04ddbeffa19b8f2fd30370aa492db
GesonAnko <59220704+Geson-anko@users.noreply.github.com>
2025-01-21 20:39:14 +0900

    スペース数を任意に設定可能に

--- src/completionProviders/pythonPathCompletion.ts
+++ src/completionProviders/pythonPathCompletion.ts
@@ -8,28 +8,25 @@ export class PythonPathCompletionProvider implements vscode.CompletionItemProvid
     position: vscode.Position,
   ): Promise<vscode.CompletionItem[] | undefined> {
     const linePrefix = document.lineAt(position).text.slice(0, position.character);
-    console.log(1);
-    // _target_: の直後のみ補完を提供
-    if (!linePrefix.endsWith(`${HYDRA_KEYWORDS.TARGET}: `)) {
+
+    // _target_: の後に続く空白文字で発火するように条件を変更
+    if (!linePrefix.match(`${HYDRA_KEYWORDS.TARGET}:\\s*$`)) {
       return undefined;
     }
-    console.log(2);
 
     const pythonPath = vscode.workspace.getConfiguration().get<string>(HYDRA_SETTINGS.PYTHON_PATH);
     if (!pythonPath) {
       return undefined;
     }
-    console.log(3);
 
     try {
       const { stdout } = await execAsync(`"${pythonPath}" -c "${PYTHON_SCRIPTS.LIST_TOP_LEVEL_PACKAGES}"`);
-      console.log(stdout);
+
       return stdout.split("\n")
         .filter(name => name.trim())
         .map(name => {
           const item = new vscode.CompletionItem(name);
           item.kind = vscode.CompletionItemKind.Module;
-          console.log(item);
           return item;
         });
     } catch (error) {
