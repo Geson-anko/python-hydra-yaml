5884715437673ae734e25c699ecc6b995e53de67
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-01-22 15:47:49 +0900

    remove select root config file command

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,7 @@
 import * as vscode from "vscode";
-import { registerCommands } from "./commands";
 import { clearDiagnostics, initDiagnostics } from "./utils/diagnostics";
 
 export function activate(context: vscode.ExtensionContext) {
-  registerCommands(context);
   initDiagnostics(context);
 }
 
