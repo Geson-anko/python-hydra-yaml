55bb7830b9f9a41dc6dd7ba47b0d40c5feb968c3
GesonAnko <59220704+Geson-anko@users.noreply.github.com>
2025-01-25 04:41:01 +0000

    fix provider name

--- src/semanticTokenProviders/index.ts
+++ src/semanticTokenProviders/index.ts
@@ -1,5 +1,5 @@
 import * as vscode from "vscode";
-import { TargetSemanticTokensProvider } from "./targetTokenProvider";
+import { TargetSemanticTokensProvider } from "./targetSemanticTokenProvider";
 
 /**
  * Registers semantic token providers for the extension
